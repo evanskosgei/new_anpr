3f3a262c98cb0b19c8fa7cec7c320e154b0e8ce7
OnserioDonald <61907509+codydon@users.noreply.github.com>
2022-04-15 02:48:05 +0300

    anpr commit

--- anprapi/app/Http/Controllers/WatchListController.php
+++ anprapi/app/Http/Controllers/WatchListController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\WatchList;
 
 class WatchListController extends Controller
 {
     //
+    function add_to_watchlist(Request $request){
+        $v = new WatchList();
+        $v->registration_number = $request->input('reg_plate');
+        $v->owner = $request->input('owner');
+        $v->vehicle_make = $request->input('vehicle_make');
+        $v->year_of_manufacture = $request->input('model_year');
+        $v->engine_capacity = $request->input('engine_capacity');
+        $v->body_type = $request->input('body_type');
+        $v->color = $request->input('color');
+        $v->logbook_number = $request->input('logbook_number');
+        $v->engine_number = $request->input('engine_number');
+        $v->chassis_number = $request->input('chasis_number');
+        $v->save();
+        return response()->json('success');
+    }
+
+    function delete_from_watchlist($key){
+        $v = WatchList::where('registration_number', 'like', "$key")->get();
+        // dd(count($v));
+        if (count($v) > 0){
+        $v->delete();
+        return response()->json('success');
+        }
+        else{
+            return response()->json('error');
+        }
+    }
 }
--- anprapi/routes/api.php
+++ anprapi/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\LogController;
+use App\Http\Controllers\WatchListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('vehicle/{key}', [VehicleController::class, 'search']);
 Route::get('logs', [LogController::class, 'logs']);
+Route::post('add_to_watchlist', [WatchListController::class, 'add_to_watchlist']);
+Route::get('delete_from_watchlist/{key}', [WatchListController::class, 'delete_from_watchlist']);
